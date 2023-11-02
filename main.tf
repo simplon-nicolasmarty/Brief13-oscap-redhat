67a65476b3af21d28f022beb9773c8aefb90db03
simplon-nicolasmarty <108000701+simplon-nicolasmarty@users.noreply.github.com>
2023-11-02 10:30:40 +0100

    main.tf

---
+++ main.tf
@@ -0,0 +1,179 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=3.75.0"
+    }
+    ansible = {
+      version = "~> 1.1.0"
+      source  = "ansible/ansible"
+    }
+  }
+}
+
+provider "azurerm" {
+  skip_provider_registration = true
+  features {}
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = "1-rg-NM"
+  location = "West Europe"
+}
+
+resource "azurerm_virtual_network" "vnet" {
+  name                = "nm-vnet"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+}
+
+resource "azurerm_subnet" "subnet" {
+  name                 = "nm-subn"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.0.2.0/24"]
+}
+resource "azurerm_network_security_group" "nsg" {
+  name                = "nm-nsg"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  security_rule {
+    name                       = "SSH"
+    priority                   = 100
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "22"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+
+  security_rule {
+    name                       = "AllowVNetInbound"
+    priority                   = 110
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "*"
+    source_port_range          = "*"
+    destination_port_range     = "*"
+    source_address_prefix      = "VirtualNetwork"
+    destination_address_prefix = "VirtualNetwork"
+  }
+
+  security_rule {
+    name                       = "DenyAllInbound"
+    priority                   = 4000
+    direction                  = "Inbound"
+    access                     = "Deny"
+    protocol                   = "*"
+    source_port_range          = "*"
+    destination_port_range     = "*"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+
+  security_rule {
+    name                       = "AllowAllOutbound"
+    priority                   = 100
+    direction                  = "Outbound"
+    access                     = "Allow"
+    protocol                   = "*"
+    source_port_range          = "*"
+    destination_port_range     = "*"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+}
+
+# Associate the NSG to the subnet
+resource "azurerm_subnet_network_security_group_association" "assoc" {
+  subnet_id                 = azurerm_subnet.subnet.id
+  network_security_group_id = azurerm_network_security_group.nsg.id
+}
+
+resource "azurerm_public_ip" "public_ip" {
+  name                    = "nm-ip"
+  location                = azurerm_resource_group.rg.location
+  resource_group_name     = azurerm_resource_group.rg.name
+  allocation_method       = "Static"
+}
+
+resource "azurerm_network_interface" "nic" {
+  name                = "nm-nic"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.subnet.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.public_ip.id
+  }
+}
+
+resource "azurerm_linux_virtual_machine" "vm" {
+  name                = "nm-vm"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  size                = "Standard_D4s_v3"
+  admin_username      = "nemesys"
+  network_interface_ids = [
+    azurerm_network_interface.nic.id,
+  ]
+
+  admin_ssh_key {
+    username   = "nemesys"
+    public_key = file("~/.ssh/id_rsa.pub")
+  }
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Premium_LRS"
+  }
+
+  source_image_reference {
+    publisher = "RedHat"
+    offer     = "RHEL"
+    sku       = "8-lvm-gen2"
+    version   = "latest"
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "sudo dnf -y update",
+      "sudo dnf install -y ansible",
+      "echo '[localhost]' > inventory.ini",
+      "sudo ansible-galaxy role install RedHatOfficial.rhel8_anssi_bp28_intermediary",
+      "sudo yum install -y scap-workbench",
+      "echo '- hosts: all\n  roles:\n     - { role: RedHatOfficial.rhel8_anssi_bp28_intermediary }' > playbook.yml",
+      "sudo ansible-playbook -i 'localhost,' -c local playbook.yml",
+      # ajouter cette ligne pour monter le score à 91% "sudo oscap xccdf eval --profile hipaa --remediate /usr/share/xml/scap/ssg/content/ssg-rhel8-ds.xml",
+      "oscap xccdf eval --report report.html --profile hipaa /usr/share/xml/scap/ssg/content/ssg-rhel8-ds.xml",
+    ]
+
+    connection {
+      type        = "ssh"
+      user        = "nemesys"
+      private_key = file("~/.ssh/id_rsa")
+      host        = azurerm_public_ip.public_ip.ip_address
+      timeout     = "15m" 
+    }
+  }
+}
+resource "ansible_host" "vm" {
+  name = azurerm_linux_virtual_machine.vm.name
+  groups = ["webservers"]
+  variables = {
+    ansible_ssh_private_key_file = "~/.ssh/id_rsa",
+    ansible_ssh_host = azurerm_public_ip.public_ip.ip_address,
+    ansible_ssh_user = "nemesys"
+  }
+}
+
+output "public_ip_address" {
+  value = azurerm_public_ip.public_ip.ip_address
+  description = "L'adresse IP publique de la machine virtuelle"
+}
